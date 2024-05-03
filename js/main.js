9009d82b457065b77f486f885d50d58235210b2a
celerydog <lizzy.eden@gmail.com>
2024-05-03 13:41:13 +1200

    added button and p for fact output to html
    added random fact generator function upon click

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 
+document.getElementById('factButton').addEventListener('click',uselessFactGenerator)
+
+function uselessFactGenerator() {
+    const url = 'https://uselessfacts.jsph.pl/api/v2/facts/random'
+    fetch(url) 
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        document.getElementById('placeForFacts').innerText = data.text
+        document.getElementById('factButton').value = `wow that's useless, gimme another`
+    })
+    .catch(err => {
+        console.log(`error ${err}`)
+    })
+}
